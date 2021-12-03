2a18e1db094b93de82a4319b57036c38bbfc4539
Uğur <77697813+ugurs061@users.noreply.github.com>
2021-12-03 17:19:29 +0300

    Resim ve belge açma-kaydetme-renk değiştirme- yazı tipi işlemleri

---
+++ proje3/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿
+namespace proje3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog2 = new System.Windows.Forms.SaveFileDialog();
+            this.colorDialog2 = new System.Windows.Forms.ColorDialog();
+            this.panel2 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(764, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(792, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "GÖZAT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(22, 44);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(764, 417);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(32, 467);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(181, 42);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "BELGE AÇ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(219, 467);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(181, 42);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "BELGE KAYDET";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(406, 467);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(181, 41);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "YAZI TİPİ DEĞİŞTİR";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(605, 468);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(181, 41);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "RENK DEĞİŞTİR";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Metin Belgesi|*.txt";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "Metin Belgesi|*.txt";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(945, 44);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(490, 403);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(945, 453);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(116, 42);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "RESİM AÇ";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(1111, 453);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(137, 42);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "RESİM KAYDET";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(1284, 453);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(151, 42);
+            this.button8.TabIndex = 10;
+            this.button8.Text = "RENK DEĞİŞTİR";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(1441, 453);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(46, 42);
+            this.panel1.TabIndex = 11;
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            this.openFileDialog2.Filter = "Resim Belgeleri|*.bmp;*.jpeg;*.jpg;*.png;*.gif";
+            // 
+            // saveFileDialog2
+            // 
+            this.saveFileDialog2.Filter = "Resim Belgeleri|*.bmp;*.jpeg;*.jpg;*.png;*.gif";
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(1493, 453);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(46, 42);
+            this.panel2.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1582, 498);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog2;
+        private System.Windows.Forms.ColorDialog colorDialog2;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ proje3/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proje3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //global değişken(Bütün alt fonksiyonlarda kullanılan değişkenlerdir. Fonksiyonlar haberleşecekse global değişkendir.) - 
+        //yerel değişken(sadece ilgili fonksiyonların içinde kullanılan değişkenlere denir.) tanımları sınavda sorulabilir.
+
+        //RESİM TUTMAK İÇİN NESNE TANIMLAMA
+        Bitmap resim;//resim içindeki renk bilgisi burada tutulacak
+        private void button1_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();//dosya gezgini dialog kutusu gösteriliyor
+            textBox1.Text = folderBrowserDialog1.SelectedPath;//seçilen yol
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();//dosya açma dialog kutusu gösteriliyor
+            textBox1.Text = openFileDialog1.FileName;//dosya adı textbox da gösteriliyor
+            richTextBox1.LoadFile(openFileDialog1.FileName,RichTextBoxStreamType.PlainText);//dosya yükleme işlemi
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();//dosya kaydetme dialog kutusunu gösterme
+            textBox1.Text = saveFileDialog1.FileName;//dosya adı textbox da gösteriliyor
+            richTextBox1.SaveFile(saveFileDialog1.FileName,RichTextBoxStreamType.PlainText);//dosya kaydetme işlemi
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            fontDialog1.ShowDialog();//yazı tipi dialog kutusu aktif ediliyor
+            //richTextBox1.Font = fontDialog1.Font;//yazı tipi değiştiriliyor
+            richTextBox1.SelectionFont= fontDialog1.Font;//seçilen yazının tipi değiştiriliyor
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();//renk dialog kutusu gösteriliyor
+            textBox1.Text = colorDialog1.Color.ToString();//renk gösterme işlemi
+            //richTextBox1.ForeColor = colorDialog1.Color;//renk değiştirme işlemi
+        richTextBox1.SelectionColor = colorDialog1.Color;//seçilen yazının renk değiştirme işlemi
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            openFileDialog2.ShowDialog();//dialog kutusu gösterme işlemi
+            pictureBox1.Image = Image.FromFile(openFileDialog2.FileName);//dosyadan resim yükleme işlemi
+            resim = new Bitmap(openFileDialog2.FileName);//resim bilgisi okunuyor
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            saveFileDialog2.ShowDialog();//dialog kutusu gösterme işlemi
+            pictureBox1.Image.Save(saveFileDialog2.FileName);//resim kaydetme
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            colorDialog2.ShowDialog();//renk dialog kutusu gösteriliyor
+            panel1.BackColor = colorDialog2.Color;//renk gösterme
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            int x, y;
+
+            if (e.Button != MouseButtons.Left)//sol tuşa basılı tutulmadıysa
+                return;//geri dön,bu satırdan sonraki kodlar çalışmaz
+
+            x = (resim.Width * e.X / pictureBox1.Width);//koordinat hesaplama
+            y= (resim.Height * e.Y / pictureBox1.Height);
+
+   
+            panel2.BackColor = resim.GetPixel(x,y);//x,y koordinatındaki renk bilgisi alınıyor
+            resim.SetPixel(x,y,Color.Red);//x,y koordinatını seçilen renk ile boyama
+
+            pictureBox1.Image = resim;//boyanan resmi gösterme
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
