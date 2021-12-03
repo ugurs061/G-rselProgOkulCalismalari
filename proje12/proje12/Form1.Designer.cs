b01a1499cd800e29a4c235229f341289dd8033e9
Uğur <77697813+ugurs061@users.noreply.github.com>
2021-12-03 18:21:26 +0300

    dosya okuma yazma işlemi

---
+++ proje12/proje12/Form1.Designer.cs
@@ -0,0 +1,326 @@
+﻿
+namespace proje12
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(106, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(262, 26);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "bilgi.txt";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Belge";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(382, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 32);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Oluştur";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Kayıt Sayısı";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(106, 41);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(56, 26);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "0";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(106, 73);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(56, 26);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Kayıt No";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(106, 110);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(290, 26);
+            this.textBox4.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 110);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "İsim";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(106, 142);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(290, 26);
+            this.textBox5.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 142);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Soyisim";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 172);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Telefon";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(106, 175);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(290, 26);
+            this.textBox6.TabIndex = 12;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(106, 207);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(290, 26);
+            this.textBox7.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 204);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(66, 20);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "E-Posta";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(106, 241);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(56, 32);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Geri";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(168, 241);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(60, 32);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "Ekle";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(234, 241);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(88, 32);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "Güncelle";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(328, 241);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(68, 32);
+            this.button5.TabIndex = 18;
+            this.button5.Text = "İleri";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.White;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Location = new System.Drawing.Point(106, 279);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(442, 174);
+            this.richTextBox1.TabIndex = 19;
+            this.richTextBox1.Text = "";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(106, 459);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(442, 32);
+            this.button6.TabIndex = 20;
+            this.button6.Text = "Dosya Bilgileri";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(468, 6);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(80, 32);
+            this.button7.TabIndex = 21;
+            this.button7.Text = "Aç";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Metin Belgesi|*.txt";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "Metin Belgesi|*.txt";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(601, 607);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "..::Dosya Okuma-Yazma İşlemi::..";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ proje12/proje12/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;//dosya okuma/yazma işlemi için referans
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proje12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<string> dosya;//dosyadan okunan bilgileri tutmak için
+        int kayıtsayısı = 0;
+        int kayıtno = 0;
+
+        public void dosyadanoku()
+        { 
+        
+        }
+
+        public void dosyayabilgiyaz()
+        { 
+        
+        
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+            //Dosya yolu-adı seçiliyor
+            saveFileDialog1.ShowDialog();//dialog kutusu gösteriliyor
+            textBox1.Text = saveFileDialog1.FileName;//dosya adı
+  
+            dosya = File.ReadAllLines(textBox1.Text).ToList() ;
+            //dosya oluşturma işlemi
+
+            dosya.Add(DateTime.Now.ToLongTimeString());//günün tarihi
+            dosya.Add(DateTime.Now.ToLongTimeString());//değiştirme tarihi
+            dosya.Add("0");//kayıt sayısı 0
+            dosya.Add("#############################");//ayraç-dosya bilgisi sonu
+
+            File.WriteAllLines(saveFileDialog1.FileName,dosya,Encoding.UTF8);//dosyayı kaydetme
+
+            kayıtno = 0;
+            kayıtsayısı = 0;
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();//dialog kutusu gösteriliyor
+            textBox1.Text = openFileDialog1.FileName;//dosya adı
+
+            dosya = File.ReadAllLines(textBox1.Text).ToList();
+            richTextBox1.AppendText("Dosya Oluşturma Tarihi:" + dosya[0]+"\n");//oluşturma tarihi
+            richTextBox1.AppendText("Dosya Değiştirme Tarihi:" + dosya[1] + "\n");//değiştirme tarihi
+            richTextBox1.AppendText("Dosya Değiştirme Tarihi:" + dosya[2] + "\n");//Kayıt sayısı
+
+            kayıtsayısı = Convert.ToInt16(dosya[2]);//kayıtsayısı
+            kayıtno = Convert.ToInt16(dosya[2]);//kayıtno
+
+            textBox2.Text = kayıtsayısı.ToString();
+            textBox3.Text = kayıtno.ToString();
+
+        }
+
+
+     
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            dosya = File.ReadAllLines("bilgi.txt", Encoding.UTF8).ToList();//dosyadan satır satır bilgi okuma
+            richTextBox1.AppendText("Dosya Oluşturma Tarihi:"+dosya[0]);//oluşturma tarihi
+            richTextBox1.AppendText("Dosya Değiştirme Tarihi:" + dosya[1]);//değiştirme tarihi
+            richTextBox1.AppendText("Dosya Değiştirme Tarihi:" + dosya[2]);//Kayıt sayısı
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            dosya.Add(textBox4.Text);//dosyay bilgi ekleme-isim
+            dosya.Add(textBox5.Text);//dosyay bilgi ekleme-soyisim
+            dosya.Add(textBox6.Text);//dosyay bilgi ekleme-telefon
+            dosya.Add(textBox7.Text);//dosyay bilgi ekleme-eposta
+
+            kayıtsayısı++;//kayıt sayısı 1 arttı
+            kayıtno = kayıtsayısı;
+            textBox2.Text = kayıtsayısı.ToString();
+            textBox3.Text = kayıtno.ToString();
+
+            dosya[1]= DateTime.Now.ToLongTimeString();//değiştirme tarihi
+            dosya[2] = kayıtsayısı.ToString();//kayıtsaıyısı güncellendi
+
+            File.WriteAllLines("bilgi.txt", dosya, Encoding.UTF8);//dosyadaki bilgileri bilgisayara kaydetme
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int index;
+
+            kayıtno--;
+            if (kayıtno == 0)
+                kayıtno = 1;
+            textBox3.Text = kayıtno.ToString();
+
+         
+             index = kayıtno * 4;
+
+            textBox4.Text = dosya[index];
+            textBox5.Text = dosya[index+1];
+            textBox6.Text = dosya[index+2];
+            textBox7.Text = dosya[index+3];
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int index;
+
+            kayıtno++;
+            if (kayıtno > kayıtsayısı)
+                kayıtno = kayıtsayısı;
+
+            textBox3.Text = kayıtno.ToString();
+
+            index = kayıtno * 4;
+
+            textBox4.Text = dosya[index];
+            textBox5.Text = dosya[index + 1];
+            textBox6.Text = dosya[index + 2];
+            textBox7.Text = dosya[index + 3];
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int index = kayıtno * 4;
+            dosya[index] = textBox4.Text;
+            dosya[index+1] = textBox5.Text;
+            dosya[index+2] = textBox6.Text;
+            dosya[index+3] = textBox7.Text;
+            dosya[1] = DateTime.Now.ToLongTimeString();//değiştirme tarihi
+
+            File.WriteAllLines("bilgi.txt", dosya, Encoding.UTF8);//dosyadaki bilgileri bilgisayara kaydetme
+        }
+    }
+}
