349bfa1007cdc83cccd58e0183d4845b0d8199f6
Uğur <77697813+ugurs061@users.noreply.github.com>
2021-12-03 18:20:07 +0300

    resmi tonlarına ayırma

---
+++ proje11/proje11/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace proje11
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(318, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(633, 6);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(789, 6);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(633, 162);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Resim Belgeleri|*.png;*.jpeg;*.jpg;*.bmp";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "Resim Belgeleri|*.png;*.jpeg;*.jpg;*.bmp";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 318);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Resim Aç";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 318);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Resim Kaydet";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(191, 318);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(93, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Tonlarına Ayır";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Red;
+            this.panel1.Location = new System.Drawing.Point(290, 318);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(23, 23);
+            this.panel1.TabIndex = 8;
+            this.panel1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDoubleClick);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
+            this.pictureBox6.Location = new System.Drawing.Point(789, 162);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 9;
+            this.pictureBox6.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1021, 383);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox6;
+    }
+}
+
---
+++ proje11/proje11/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proje11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Global Değişkenler
+        int w, h;//resim genişliği,yüksekliği
+        Bitmap resim,kırmızı, yeşil, mavi, gri, negatif;//renk tonları
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            int x, y;
+            if (e.Button != MouseButtons.Left)
+                return;//sol tuş basılı değilse birşey yapma/geri dön
+
+            x = (e.X * w) / pictureBox1.Width;
+            y= (e.Y*h) / pictureBox1.Height;
+
+            resim.SetPixel(x-1, y-1, panel1.BackColor);//boyama işlemi
+            resim.SetPixel(x-1, y, panel1.BackColor);//boyama işlemi
+            resim.SetPixel(x-1, y+1, panel1.BackColor);//boyama işlemi
+            resim.SetPixel(x,y-1,panel1.BackColor);//boyama işlemi
+            resim.SetPixel(x, y, panel1.BackColor);//boyama işlemi
+            resim.SetPixel(x, y+1, panel1.BackColor);//boyama işlemi
+            resim.SetPixel(x +1, y - 1, panel1.BackColor);//boyama işlemi
+            resim.SetPixel(x +1, y, panel1.BackColor);//boyama işlemi
+            resim.SetPixel(x + 1, y + 1, panel1.BackColor);//boyama işlemi
+
+            pictureBox1.Image = resim;//resim görüntüleme
+
+        }
+
+        private void panel1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            colorDialog1.ShowDialog();//dialog gösterme
+            panel1.BackColor = colorDialog1.Color;//renk atama
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int i, j;//ddöngü değişkeni
+            kırmızı = new Bitmap(w,h);//boş resim oluşturuldu
+            yeşil = new Bitmap(w, h);//boş resim oluşturuldu
+            mavi = new Bitmap(w, h);//boş resim oluşturuldu
+            gri = new Bitmap(w, h);//boş resim oluşturuldu
+            negatif = new Bitmap(w, h);//boş resim oluşturuldu
+            int r, g, b,ort;//renk tonları
+          
+            //resim işleme döngü yapısı
+            for (i = 0; i < w; i++)
+            {
+                for (j = 0; j < h; j++)
+                {
+                    r = resim.GetPixel(i,j).R;//kırmızı renk tonu 
+                    g= resim.GetPixel(i, j).G;//yeşil renk tonu  
+                    b= resim.GetPixel(i, j).B;//mavi renk tonu  
+
+                    kırmızı.SetPixel(i,j,Color.FromArgb(r,0,0));//bir pixeli boyamak için kullanılır
+                    yeşil.SetPixel(i, j, Color.FromArgb(0, g, 0));//bir pixeli boyamak için kullanılır
+                    mavi.SetPixel(i, j, Color.FromArgb(0, 0, b));//bir pixeli boyamak için kullanılır
+
+                    ort = (r + g + b) / 3;
+                    gri.SetPixel(i, j, Color.FromArgb(ort, ort,ort));//bir pixeli boyamak için kullanılır
+
+                    //negatif
+                    r = 255 - r;
+                    g = 255 - g;
+                    b = 255 - b;
+
+                    negatif.SetPixel(i, j, Color.FromArgb(r, g, b));//bir pixeli boyamak için kullanılır
+                }
+            }
+
+            //resim gösterme
+            pictureBox2.Image = negatif;
+            pictureBox3.Image = kırmızı;
+            pictureBox4.Image = yeşil;
+            pictureBox5.Image = mavi;
+            pictureBox6.Image = gri;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();//dialog gösterme
+            pictureBox1.Image.Save(openFileDialog1.FileName);//resim kaydetme
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();//dialog gösterme
+            pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);//dosyadan resim yükleme
+            resim =new Bitmap(pictureBox1.Image);//resim yükleme
+            w = resim.Width;//resim genişliği
+            h = resim.Height;//resim yüksekliği
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            resim = new Bitmap(pictureBox1.Image);//resim yükleme
+            w = resim.Width;//resim genişliği
+            h = resim.Height;//resim yüksekliği
+        }
+
+        
+
+
+
+    }
+}
